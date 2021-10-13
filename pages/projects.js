4a7b5faa9959a7b120ec642dbf64419de33fd6c3
yudhi kusuma <yudhie123@gmail.com>
2021-10-13 21:36:29 +0700

    Experience

---
+++ pages/projects.js
@@ -0,0 +1,11 @@
+import React from "react";
+import ContainerBlock from "../components/ContainerBlock";
+import Projects from "../components/Projects";
+
+export default function projects() {
+    return (
+        <ContainerBlock title="Projects - Yudhi Kusuma">
+            <Projects></Projects>
+        </ContainerBlock>
+    )
+}
